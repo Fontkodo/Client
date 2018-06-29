54c7e47133867f4a159d7c6f58735929ebc88d37
Fontkodo <alexgriffinsrq@gmail.com>
2018-06-29 11:02:31 -0400

    Implemented NetString

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,10 +6,12 @@ import java.net.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
+import fontkodo.netstring.NetString;
+
 public class Client {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ParseException {
-		Socket socket = new Socket("localhost", 8353);
+		Socket socket = new Socket("localhost", 9999);
 		JSONObject ob = new JSONObject();
 		ob.put("Dog", 123);
 		ob.put("Cat", "Meow");
@@ -17,12 +19,10 @@ public class Client {
 		OutputStreamWriter obw = new OutputStreamWriter(socket.getOutputStream());
 		obw.write(ob.toString());
 		obw.flush();
-		InputStreamReader ipr = new InputStreamReader(socket.getInputStream());
-		char [] stuff = new char[100_000];
-		int n = ipr.read(stuff);
-		CharArrayReader car = new CharArrayReader(stuff, 0, n);
+		InputStream ipr = new BufferedInputStream(socket.getInputStream());
+		String content = NetString.readString(ipr);
 		JSONParser p = new JSONParser();
-		System.out.println(p.parse(car));
+		System.out.println(p.parse(content));
 		obw.close();
 		socket.close();
 
